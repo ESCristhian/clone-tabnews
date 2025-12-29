b185765ad6d2db950eb23da45db08a685bea564d
Cristhian Elias <cristhianeliassales@gmail.com>
2025-12-29 11:20:42 +0000

    finaliza endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -11,9 +11,11 @@ async function status(request, response) {
   const maxConect = await database.query("SHOW max_connections;");
   const maxConectValue = parseInt(maxConect.rows[0].max_connections);
 
-  const openConect = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const openConect = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const openConectValue = parseInt(openConect.rows[0].count);
 
   response.status(200).json({
